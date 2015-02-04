4481e4fe3a1e0ad45f570fd0a6a6683a58a1cc40
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-03 22:45:12 -0700

    updated faq page

---
+++ app/controllers/common_questions_controller.rb
@@ -0,0 +1,12 @@
+class CommonQuestionsController < ApplicationController
+  def index
+    @questions_array =  [
+      {'question' => 'What is gCamp?',
+       'answer' => 'gCamp is an awesome tool that is going to change your life. gCamp is your one stop shop to organize all your tasks. You\'ll also be able to track comments that you and others make. gCamp may eventually replace all need for paper and pens in the entire world.'},
+      {'question' => 'How do I join gCamp?',
+       'answer' => 'As soon as it\'s ready for the public, you\'ll see a sign up link in the upper right. Once that\'s there, just click it and fill in the form!'},
+      {'question' => 'When will gCamp be finished?',
+       'answer' => 'gCamp is a work in progress. That being said, it should be fully functional in the next few weeks. Functional. Check in daily for new features and awesome functionality. It\'s going to blow your mind. Organization is just a click away. Amazing!'}]
+
+  end
+end
---
+++ app/models/common_questions.rb
@@ -0,0 +1,3 @@
+class CommonQuestions
+
+end
