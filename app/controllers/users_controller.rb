b0f66efbbd5ac58dae0907f38f4956364634d6a4
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-17 15:36:58 -0700

    updated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,22 @@ class UsersController < ApplicationController
     redirect_to users_path(user)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to users_path(user)
+      flash[:notice] = "User was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+
+
   private
 
   def user_params
