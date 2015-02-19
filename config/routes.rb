d30a762f5dd88fc8e51e12bddafe8de443c369c5
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-18 21:09:39 -0700

    added "projects" route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :tasks
   resources :users
+  resources :projects
 
    get '/terms' => 'terms#index'
    get '/about' => 'about#index'
