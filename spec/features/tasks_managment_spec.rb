e5bd3f6d55da582eb89c5220122d608750666e25
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-22 20:42:44 -0700

    test for tasks

--- spec/features/tasks_managment_spec.rb
+++ spec/features/tasks_managment_spec.rb
@@ -1,5 +1,36 @@
 require "rails_helper"
 
-describe
+describe "the user interacting with: task crud" do
+  it "is complete" do
+    visit tasks_path
+    expect(page).to have_content "Tasks"
 
-"the user"
+    click_on "New Task"
+
+    fill_in "Description", with: "hike"
+    fill_in "Due date", with: "02/03/2015"
+
+    click_on "Create Task"
+
+    expect(page).to have_content "hike"
+    expect(page).to have_content "03/02/2015"
+
+    click_on "Edit"
+
+    fill_in "Description", with: "hike!"
+    fill_in "Due date", with: "04/05/2015"
+    check "Complete"
+    click_on "Update Task"
+
+    expect(page).to have_content "hike!"
+    expect(page).to have_content "05/04/2015"
+
+    visit tasks_path
+
+    click_on "Delete"
+
+    expect(page).to_not have_content "hike"
+    expect(page).to_not have_content "02/02/2015"
+
+  end
+end
