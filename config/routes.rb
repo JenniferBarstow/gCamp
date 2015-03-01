c5a7d9f10a2d6980a60d6e877ce9f1e058213ff4
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-01 16:41:30 -0700

    added registrations and authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
    get '/about' => 'about#index'
    get '/faq' => 'common_questions#index'
    #get '/tasks' => 'tasks#index'
-
+   get '/sig-up', to: 'registrations#new'
+   post '/sign-up', to: 'registrations#create'
+   get '/sig-in', to: 'authentication#new'
+   post '/sign-in', to: 'authentication#create'
+   get '/sign-out', to: 'authentication#destroy'
  end
