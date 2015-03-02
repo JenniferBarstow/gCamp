eb6934a9262a5df25880fbd695c593dc6d50a6cd
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-01 19:26:34 -0700

    fixed edit. add : before locals

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,25 +1,25 @@
 class AuthenticationController < ApplicationController
   skip_before_action :ensure_current_user
 
+  def destroy
+    session.clear
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
   def new
   end
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       flash[:notice] = "You have successfully signed in"
       redirect_to root_path
     else
       flash[:error] = "Email / Password combination is invalid"
       render :new
     end
-  end
-
-  def destroy
-    session.clear
-    flash[:notice] = "You have successfully logged out"
-    redirect_to root_path
   end
 
 end
