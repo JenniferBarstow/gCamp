571f0c94646aa8aebd1a9b89f8869145f84fcf0f
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-01 17:04:55 -0700

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+  has_secure_password
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
