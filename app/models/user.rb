c9d9d65aed24b41437cc0ff9a9c7a95dbf55fe1e
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-06 14:04:46 -0700

    migration for memberships, set up membership model and associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+  has_many :memberships
+  has_many :projects, through: :memberships
   has_secure_password
 
   def full_name
