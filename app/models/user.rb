aeb77226a4b97e32a5b173bd6c20e3d1555e8c5d
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-09 15:39:15 -0600

    rearranged user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+
   has_many :memberships
   has_many :projects, through: :memberships
-  has_secure_password
+
 
   def full_name
     "#{first_name} #{last_name}"
