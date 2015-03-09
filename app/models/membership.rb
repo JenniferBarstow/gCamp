56c0619fbecc05116893839e628e51a0b1b081fa
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-09 15:46:12 -0600

    added uniqueness validation to membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,8 @@
 class Membership < ActiveRecord::Base
   ROLE = ["Member", "Owner"]
   validates :user, presence: true
+  validates_uniqueness_of :user, scope: :project, message: "has already been added to this project"
+
   belongs_to :user
   belongs_to :project
 
