16700732be20069d877f84c2ae76369daaa0a3a7
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-11 14:34:00 -0600

    registration spec passing

---
+++ spec/features/registrations_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'User should be able to log into gCamp and access other pages' do
+
+  scenario 'should be able to login to gCamp from the welcome page' do
+    visit root_path
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Bruce'
+    fill_in 'Last Name', with: 'Wayne'
+    fill_in 'Email', with: 'brucewayne@batman.com'
+    fill_in 'Password', with: 'robin'
+    fill_in 'Password Confirmation', with: 'robin'
+    within '.form' do
+      click_on 'Sign Up'
+    end
+    within '.alert-success' do
+      expect(page).to have_content 'You have successfully signed up'
+    end
+  end
+
+  scenario 'should redirect user to root path when signing up' do
+    visit root_path
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Bruce'
+    fill_in 'Last Name', with: 'Wayne'
+    fill_in 'Email', with: 'brucewayne@batman.com'
+    fill_in 'Password', with: 'robin'
+    fill_in 'Password Confirmation', with: 'robin'
+    within '.form' do
+      click_on 'Sign Up'
+    end
+    expect(current_path).to eq(root_path)
+  end
+
+end
