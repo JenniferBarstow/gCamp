8db61b15b4aabdd3e92db0823d447b21a5c89757
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-11 16:43:57 -0600

    def create in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,12 @@
 class CommentsController < ApplicationController
 
+  def create
+    task = Task.find(params[:task_id])
+    comment = Comment.new(params.require(:comment).permit(:description).merge(
+    task_id => task.id
+    user_id  => user.id
+    ))
+    comment.save
+    redirect_to project_task_path(task.project_id, task)
+  end
 end
