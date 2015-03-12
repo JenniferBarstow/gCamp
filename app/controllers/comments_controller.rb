383ddb486027d70dbb93a1b2008f8aac76088667
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-11 22:30:52 -0600

    fixed create in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
 
   def create
-    task = Task.find(params[:task_id])
-    comment = Comment.new(params.require(:comment).permit(:description).merge(
-    task_id => task.id
-    user_id  => user.id
-    ))
-    comment.save
-    redirect_to project_task_path(task.project_id, task)
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to project_task_path(@task.project_id, @task)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :task_id, :message)
+  end
+
 end
