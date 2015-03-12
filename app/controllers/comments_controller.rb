76996c8dc83f3af4ec462acfa6a2770cbc12635e
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-12 13:35:33 -0600

    got comments to show up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,14 @@ class CommentsController < ApplicationController
     @task = Task.find(params[:task_id])
     @comment = @task.comments.new(comment_params)
     @comment.user_id = current_user.id
-    if @comment.save
+    @comment.save
       redirect_to project_task_path(@task.project_id, @task)
-    else
-      render :new
     end
-  end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:user_id, :task_id, :message)
+    params.require(:comment).permit(:user_id, :task_id, :description)
   end
 
 end
