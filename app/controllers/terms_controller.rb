cbef5d2032841c47e729897976af86cc06db2993
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-12 22:18:59 -0600

    deleted duplicate about directory and added to the terms index

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,10 +1,14 @@
 class TermsController < ApplicationController
-  skip_before_action :ensure_current_user
+   skip_before_action :ensure_current_user
 
   def index
   end
 
   def about_page
+    @projects = Project.all
+    @users = User.all
+    @comments = Comment.all
+    @memberships = Membership.all
+    @tasks = Task.all
   end
-
 end
